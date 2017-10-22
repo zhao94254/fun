4aa3270af225898932a38a2c6c3c21ecef6ebf14
zhao94254 <zhao942548838@qq.com>
2017-10-22 18:17:40 +0800

    Fun code

---
+++ funcode.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# @Author  : pengyun
+
+# pythonic & one line
+
+
+
+
+
+
+
+
+# 丧心病狂的一行代码实现。
+
+# python3 -m http.server port 一行开启web服务。
+# 如果是需要分享一些东西这个功能简直不能太赞
+
+# 如果是复杂的 无法一眼读下来的代码是不应该在程序中追求这种写法的
+# from operator import sub, mul
+# fact = lambda n : 1 if n == 1 else mul(fact(n-1), n) 一行求阶乘
+# fib = lambda n : n if n < 2 else fib(n-1) + fib(n-2) 一行求fib
+
+# 一行9 9乘法表
+# print('\n'.join([ ' '.join([ "{}*{}={}".format(i, j, i*j)for j in range(1, i+1)]) for i in range(1, 10)]))
+
+# 一行把 多维数组变为一维\
+# flatten = lambda x: [i for j in x for i in flatten(j)] if isinstance(x, list) else [x]
+
+# 一行筛素数
+# prime = lambda n: [ i for i in range(2, n) if all([i%j!=0 for j in range(2, i)])]
+# prime = lambda n: [ i for i in range(2, n) if all(map(lambda j: i%j!=0, range(2, i)))]
+
+
