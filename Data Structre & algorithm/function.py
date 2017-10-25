ad64c16a5ba1a288c970b713ba377498cacef46e
zhao94254 <zhao942548838@qq.com>
2017-10-25 20:46:37 +0800

    Ds

--- Data Structre & algorithm/function.py
+++ Data Structre & algorithm/function.py
@@ -3,7 +3,9 @@
 
 def convert(n, x):
     """
-    N 进制转化， 直接使用ascii码中的字符来代替了"
+    在短网址系统会使用到这样的一个转化，将一个大的十进制数转化为一个
+    短的高进制的数。常用的转化是62进制。[Aa-Zz, 0-9]
+    N 进制转化， 直接使用ascii码中的字符来代替了。
     :param n:  N进制
     :param x:  一个十进制的数
     :return:
@@ -16,6 +18,31 @@ def convert(n, x):
         res += tmp[x % n]
         x = x//n
     print(res[::-1])
+
+
+def max_num(array):
+    """
+    最大连续子数组，考虑到全为负数的情况
+    :param array:
+    :return:
+    """
+    if len(array) == len([i for i in array if i<0]):
+        return max(array)
+    res, tmp = 0, 0
+    for i in array:
+        tmp += i
+        res = max(res, tmp)
+        if tmp < 0:
+            tmp = 0
+    return res
+
+def max_subarray(x, array):
+    """求长为x的最大子数组"""
+    tmp, res = 0, 0
+    for i in range(len(array)-x):
+        tmp = sum(array[i:i+x])/x
+        res = max(res, tmp)
+    return res
 
 
 
