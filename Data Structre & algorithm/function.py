e24413b13ec5d9b514969794215e279577c0a3aa
zhao94254 <zhao942548838@qq.com>
2017-11-04 14:39:07 +0800

    Ds

--- Data Structre & algorithm/function.py
+++ Data Structre & algorithm/function.py
@@ -54,6 +54,73 @@ def move_stack(n, start, end, tmp):
         move_stack(n-1, tmp, end, start)
 
 
+def deep_len(lst):
+    """
+    返回一个有多级嵌套列表的长度
+    >>> x = [1,2,[3,4,[3,4,6]]]
+    >>>deep_len(x)
+    7
+    :param lst
+    :return:
+    """
+    len_lst = 0
+    def helper(lst):
+        nonlocal len_lst
+        for i in lst:
+            if isinstance(i, list):
+                helper(i)
+            else:
+                len_lst += 1
+    helper(lst)
+    return len_lst
+
+def getfile(filetmp):
+    """获取指定文件夹下所有的文件"""
+    res = []
+    import os
+    def helper(filetmp):
+        for i in os.listdir(filetmp):
+            i = os.path.join(filetmp, i)
+            if os.path.isfile(i):
+                res.append(i)
+            else:
+                helper(i)
+        return res
+    return helper(filetmp)
+
+def reorder_lst(lst):
+    """将奇数放在前面，并保持相对位置不变"""
+    return [i for i in lst if i%2 ] + [i for i in lst if i%2==0 ]
+
+def josephus(n, m):
+    """约瑟夫环问题"""
+    if n == 0 or m == 0:
+        return -1
+    m -= 1
+    p = 0
+    while n:
+        p = (p+m)%len(n)
+        if len(n) == 1:
+            return n[0]
+        del n[p]
+
+
+def is_symmetrical(tree1, tree2):
+    """检查两个树是否互为镜像树"""
+    def helper(l, r):
+        if l is None and r is None:
+            return True
+        if l is None or r is None:
+            return False
+        if l.val != r.val:
+            return False
+        return helper(l.left, r.right) and helper(l.right, r.left)
+    if tree1 is None and tree2 is None:
+        return True
+    return helper(tree1.left, tree2.right)
+
+
+
 
 
 
