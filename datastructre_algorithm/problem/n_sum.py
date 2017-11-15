52b007eaf5c18b17888bbc199012d2324a12b3f0
zhao94254 <zhao942548838@qq.com>
2017-11-15 23:50:46 +0800

    update

--- datastructre_algorithm/problem/n_sum.py
+++ datastructre_algorithm/problem/n_sum.py
@@ -3,3 +3,26 @@
 # @Created on    : 17/11/15 下午7:21
 # @Author  : zpy
 # @Software: PyCharm
+
+
+def two_sum(numbers, target):
+    """ two sum """
+    for i, num in enumerate(numbers):
+        tmp = target - num
+        if tmp in numbers:
+            return [i+1, numbers.index(tmp)+1]
+    return []
+
+def three_sum(numbers, target):
+    """ three sum"""
+    for i, num in enumerate(numbers):
+        f = target - num
+        res = two_sum(numbers[i+1:], f)
+        if len(res) > 0:
+            return [i, res[0]+i, res[1]+i]
+    return []
+
+
+print(three_sum(list(range(10)), 6))
+
+
