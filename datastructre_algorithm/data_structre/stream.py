ccd952e869e702d146064bad1512a0293d60ade1
zpy <zhao942548838@qq.com>
2017-11-17 19:20:08 +0800

    update

---
+++ datastructre_algorithm/data_structre/stream.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Created on    : 17/11/17 下午4:52
+# @Author  : zpy
+# @Software: PyCharm
+
+
+class Stream:
+    def __init__(self, first, second=lambda:None):
+        assert callable(second), 'must be callable'
+        self.first = first
+        self.second = second
+
+    @property
+    def rest(self):
+        """只在第一次调用时计算一次 second"""
+        if self.second is not None:
+            print('call')
+            self._rest = self.second()
+            self.second = None
+        return self._rest
+
+
+
+s = Stream(1, lambda :Stream(2+3, lambda :Stream(1)))
+
+
+
+
+
+
+
