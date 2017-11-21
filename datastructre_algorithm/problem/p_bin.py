53c1a45d6f89973897333aedfc6d7b3f14fe157d
zpy <zhao942548838@qq.com>
2017-11-21 14:06:54 +0800

    update

--- datastructre_algorithm/problem/p_bin.py
+++ datastructre_algorithm/problem/p_bin.py
@@ -4,10 +4,6 @@
 # @Author  : zpy
 # @Software: PyCharm
 
-
-
-
-
 def count_bin(x):
     """统计x的二进制中1的个数"""
     res = 0
@@ -16,3 +12,18 @@ def count_bin(x):
             res += 1
         x >>= 1
     return res
+
+def a_to_b(a, b):
+    """a 转化为b要改变的二进制位数"""
+    return count_bin(a^b)
+
+def is_two_pow(x):
+    if x<0:
+        return False
+    return x&(x-1) == 0
+
+
+if __name__ == '__main__':
+    print(a_to_b(31,14))
+
+    print(is_two_pow(4))
