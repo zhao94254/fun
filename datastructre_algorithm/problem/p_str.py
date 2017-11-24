2089f684a5b0c68132805361ba36179c0e9631bd
zpy <zhao942548838@qq.com>
2017-11-24 13:43:52 +0800

    update

--- datastructre_algorithm/problem/p_str.py
+++ datastructre_algorithm/problem/p_str.py
@@ -18,3 +18,33 @@ def is_rotate(s1, s2):
     if s2 in s1*2:
         return True
     return False
+
+def anagrams(lst):
+    def sort_str(s):
+        return ''.join(sorted(list(s)))
+    _map = {}
+    res = []
+    for i in lst:
+        si = sort_str(i)
+        if si not in _map:
+            _map[si] = [i]
+        else:
+            _map[si].append(i)
+    for m in _map:
+        if len(_map[m]) > 1:
+            res += _map[m]
+    return res
+
+
+if __name__ == '__main__':
+
+    print(anagrams(['lint','inlt','intl','fkkk']))
+
+
+
+
+
+
+
+
+
