56cea98334c0f640cabc169d4493fe0acc3852ab
zhao94254 <zhao942548838@qq.com>
2017-12-03 14:36:54 +0800

    update

---
+++ other/modulepackage/bar/testb.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# @Author  : pengyun
+
+from ..foo import testf
+x = 'foo'
+
+def test(x):
+    print(x)
+
+test(x)
+
+print(__name__)
+
+
+
---
+++ other/modulepackage/foo/testf.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# @Author  : pengyun
+
+from ..bar.testb import test
+
+x = 'foo'
+
+def test(x):
+    print(x)
+
+test(x)
