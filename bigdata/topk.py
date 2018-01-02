fc05a33458e238c7ade61b64edfa38ff712fb8d5
zhaopengyun <zhao942548838@qq.com>
2018-01-02 18:52:24 +0800

    update

--- bigdata/topk.py
+++ bigdata/topk.py
@@ -6,5 +6,16 @@
 
 from tools import getfile
 import os
+from heapq import nlargest
+
+def count(f, mpath):
+    """ 读取f，将map保存在path中。"""
+    pass
+
+def main(path, mpath):
+    for i in getfile(path):
+        count(i, mpath)
 
 
+if __name__ == '__main__':
+    main('xx', 'xx_map')
