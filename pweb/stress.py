87fe062c3ec01c63c94501e64e5de790c33b7767
zhaopengyun <zhao942548838@qq.com>
2018-01-10 19:45:12 +0800

    count success

--- pweb/stress.py
+++ pweb/stress.py
@@ -6,8 +6,14 @@
 
 import requests
 import inspect
+import time
+import redis
 
-class Query:
+
+redis_db = redis.Redis()
+
+
+class BaseQuery:
     """
     Single test. 负责单个任务
     所有的测试函数以check_ 开头, 返回 true or false
@@ -16,6 +22,9 @@ class Query:
     def __init__(self, url):
         self.session = requests.session()
         self.url = url
+
+    def check_testa(self):
+        return False
 
     def check_test(self):
         return True
@@ -28,10 +37,28 @@ class Query:
         methods = self.getmethods()
         _check = [m for m in methods if m[0].startswith('check_')]
         all_check = len(_check)
-        success_check = len([m[1] for m in _check if m[1]])
+        print(_check)
+        success_check = len([1 for m in _check if m[1]()])
         rate = success_check / all_check
         return rate == 1, rate
 
+def job(query):
+    start = time.time()
+    try:
+        ok, rate = query.check()
+    except:
+        ok, rate = False, 0
+
+    end = time.time()
+    cost = end - start
+    # 保存成功率， 成功次数
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    q = Query('x')
+    q = BaseQuery('x')
     print(q.check())
